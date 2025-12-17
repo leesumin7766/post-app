32c5401567a10df7556e6452053e41e1824db8f9
leesumin7766 <160687820+leesumin7766@users.noreply.github.com>
2025-12-18 02:40:05 +0900

    docker-compose.yml

---
+++ src/main/java/com/sparta/postapp/domain/post/service/dto/Post.java
@@ -0,0 +1,16 @@
+package com.sparta.postapp.domain.post.service.dto;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Post {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String title;
+}
